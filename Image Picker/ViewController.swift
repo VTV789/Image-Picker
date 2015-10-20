f5c48d7f0052124585c3b2c58a145eec2715d4da
Vinh Thanh Vu <vtv@vinh.com>
2015-10-20 19:11:36 -0400

    Add okAction for users to tap "ok" to dismiss the view controller

--- Image Picker/ViewController.swift
+++ Image Picker/ViewController.swift
@@ -19,8 +19,13 @@ class ViewController: UIViewController {
         let controller = UIAlertController()
         controller.title = "Test alert"
         controller.message = "This is a test"
-        self.dismissViewControllerAnimated(true, completion: nil)
         
+        //Dismiss the view controller after the user taps "ok"
+        let okAction = UIAlertAction (title: "ok", style: UIAlertActionStyle.Default)   {
+            action in self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+        controller.addAction(okAction)
         self.presentViewController(controller, animated: true, completion: nil)
         
     }
